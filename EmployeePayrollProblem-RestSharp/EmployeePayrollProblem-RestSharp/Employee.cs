d6d8dcab0722de2bcf37b8ce5d445a044de75570
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-06-06 17:59:50 +0530

    Added file of UC4_UPDATE_SALARY_INTO_THE_JSON_SERVER

---
+++ EmployeePayrollProblem-RestSharp/EmployeePayrollProblem-RestSharp/Employee.cs
@@ -0,0 +1,19 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Bridgelabz">
+//   Copyright © 2018 Company
+// </copyright>
+// <creator Name="Dheer Singh Meena"/>
+// --------------------------------------------------------------------------------------------------------------------
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayrollProblem_RestSharp
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Salary { get; set; }
+    }
+}

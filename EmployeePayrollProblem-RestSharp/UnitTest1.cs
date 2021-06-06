c981a503f061bed6da62f7df04db663737c865a7
himanshu kholiya <kholiyahimanshu2@gmail.com>
2021-06-06 17:53:39 +0530

    Added file of UC3_ADD_MULTIPLE_EMP_INTO_JSON_SERVER

--- EmployeePayrollProblem-RestSharp/UnitTest1.cs
+++ EmployeePayrollProblem-RestSharp/UnitTest1.cs
@@ -52,5 +52,64 @@ namespace EmployeePayrollProblem_RestSharp
                 Console.WriteLine("Id: " + emp.Id + "\t" + "Name: " + emp.Name + "\t" + "Salary: " + emp.Salary);
             }
         }
+        /// <summary>
+        /// UC2 Ability to add new employee to the json file in JSON server and return the same
+        /// </summary>
+        [TestMethod]
+        public void OnCallingPostAPI_ReturnEmployeeObject()
+        {
+            //Arrange
+            ///Initialize the request for POST to add new employee
+            RestRequest request = new RestRequest("/Employees/list", Method.POST);
+            JsonObject jsonObj = new JsonObject();
+            jsonObj.Add("name", "Ritik");
+            jsonObj.Add("salary", "50000");
+            jsonObj.Add("id", "7");
+            ///Added parameters to the request object such as the content-type and attaching the jsonObj with the request
+            request.AddParameter("application/json", jsonObj, ParameterType.RequestBody);
+
+            //Act
+            IRestResponse response = client.Execute(request);
+
+            //Assert
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+            Employee employee = JsonConvert.DeserializeObject<Employee>(response.Content);
+            Assert.AreEqual("Ritik", employee.Name);
+            Assert.AreEqual("50000", employee.Salary);
+            Console.WriteLine(response.Content);
+        }
+        /// <summary>
+        /// UC3 Ability to adding multiple employees to the json file using JSON server and returns the same
+        /// </summary>
+        [TestMethod]
+        public void OnCallingPostAPIForAEmployeeListWithMultipleEMployees_ReturnEmployeeObject()
+        {
+            // Arrange
+            List<Employee> employeeList = new List<Employee>();
+            employeeList.Add(new Employee { Name = "Radha", Salary = "85536" });
+            employeeList.Add(new Employee { Name = "Watson", Salary = "120123" });
+            employeeList.Add(new Employee { Name = "Christiano Ronaldo", Salary = "123456" });
+            //Iterate the loop for each employee
+            foreach (var emp in employeeList)
+            {
+                ///Initialize the request for POST to add new employee
+                RestRequest request = new RestRequest("/Employees/list", Method.POST);
+                JsonObject jsonObj = new JsonObject();
+                jsonObj.Add("name", emp.Name);
+                jsonObj.Add("salary", emp.Salary);
+                ///Added parameters to the request object such as the content-type and attaching the jsonObj with the request
+                request.AddParameter("application/json", jsonObj, ParameterType.RequestBody);
+
+                //Act
+                IRestResponse response = client.Execute(request);
+
+                //Assert
+                Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+                Employee employee = JsonConvert.DeserializeObject<Employee>(response.Content);
+                Assert.AreEqual(emp.Name, employee.Name);
+                Assert.AreEqual(emp.Salary, employee.Salary);
+                Console.WriteLine(response.Content);
+            }
+        }
     }
 }
